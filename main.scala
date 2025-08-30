09ee2436fb7b1481ffd21a7cc0b4b53a5f7720db
Jakub Kozłowski <kubukoz@gmail.com>
2025-08-30 23:49:51 +0200

    reorg

--- main.scala
+++ main.scala
@@ -6,29 +6,81 @@ import tui.widgets.*
 import tui.crossterm.CrosstermJni
 import tui.crossterm.KeyModifiers
 
-@main def app: Unit = util.boundary[Unit] {
-  withTerminal { (jni, terminal) =>
-    while (true) {
-      terminal.draw { f =>
-        f.renderWidget(
-          BlockWidget(
-            title = Some(Spans(Array(Span("app", Style.DEFAULT)))),
+@main def app: Unit = {
+  var selected = 0
+  extension (i: Int) {
+    def clamped(range: Range): Int =
+      if (i < range.start)
+        range.end - 1
+      else if (i >= range.end)
+        range.start
+      else
+        i
+  }
+  var items = List(
+    "beer",
+    "taxes",
+  )
+  val choice = util.boundary[Option[String]] {
+    withTerminal { (jni, terminal) =>
+      while (true) {
+        terminal.draw { f =>
+          val block = BlockWidget(
+            title = Some(Spans(Array(Span("select your poison", Style.DEFAULT)))),
             borders = Borders.ALL,
-          ),
-          terminal.viewport.area,
-        )
-      }
+          )
 
-      jni.read() match {
-        case key: tui.crossterm.Event.Key =>
-          key.keyEvent.code match {
-            case char: tui.crossterm.KeyCode.Char
-                if char.c() == 'c' && (key.keyEvent().modifiers().bits() == KeyModifiers.CONTROL) =>
-              util.boundary.break()
-            case _ => ()
-          }
-        case _ => ()
+          val list = ListWidget(
+            block = Some(block),
+            items =
+              items
+                .map(i =>
+                  ListWidget.Item(
+                    content = Text(
+                      Array(
+                        Spans(
+                          Array(Span(i, Style.DEFAULT))
+                        )
+                      )
+                    )
+                  )
+                )
+                .toArray,
+            style = Style.DEFAULT,
+            startCorner = Corner.TopLeft,
+            highlightStyle = Style.DEFAULT.fg(Color.Red),
+            highlightSymbol = Some("> "),
+          )
+
+          f.renderStatefulWidget(
+            list,
+            terminal.viewport.area,
+          )(ListWidget.State(offset = 0, selected = Some(selected)))
+        }
+
+        jni.read() match {
+          case key: tui.crossterm.Event.Key =>
+            key.keyEvent.code match {
+              case char: tui.crossterm.KeyCode.Char
+                  if char.c() == 'q' || (char
+                    .c() == 'c' && (key.keyEvent().modifiers().bits() == KeyModifiers.CONTROL)) =>
+                util.boundary.break(None)
+              case char: tui.crossterm.KeyCode.Up =>
+                selected = (selected - 1).clamped(items.indices)
+              case char: tui.crossterm.KeyCode.Down =>
+                selected = (selected + 1).clamped(items.indices)
+              case keys: tui.crossterm.KeyCode.Enter => util.boundary.break(Some(items(selected)))
+              case _                                 => ()
+            }
+          case _ => ()
+        }
       }
     }
+
+    None
+  }
+
+  choice.foreach { c =>
+    println(s"congrats, you've chosen ${c}!")
   }
 }
