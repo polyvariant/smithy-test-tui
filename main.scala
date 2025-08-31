f8bf30067ff0b737f8ede51f133f8aa9ee9a40fb
Jakub Kozłowski <kubukoz@gmail.com>
2025-08-31 13:28:25 +0200

    Add ticks

--- main.scala
+++ main.scala
@@ -1,4 +1,5 @@
 //> using dep "com.olvind.tui::tui:0.0.7"
+//> using dep "com.softwaremill.ox::core:1.0.0"
 //> using scala 3.7.3-RC2
 //> using options -no-indent
 import tui.*
@@ -8,8 +9,16 @@ import tui.crossterm.KeyModifiers
 import java.util.UUID
 import tui.widgets.ParagraphWidget.Wrap
 import util.chaining.*
+import java.time.Instant
+import scala.concurrent.duration.*
+
+enum Event {
+  case Tick
+}
 
 @main def app: Unit = {
+  val channel = ox.channels.Channel.bufferedDefault[Event]
+
   var selected = 0
   extension (i: Int) {
     def clamped(range: Range): Int =
@@ -27,108 +36,129 @@ import util.chaining.*
 
   val choice = util.boundary[Option[String]] {
     withTerminal { (jni, terminal) =>
-      while (true) {
-        val item = items(selected)
+      def renderLoop =
+        while (true) {
+          channel.receive()
 
-        terminal.draw { f =>
-          Layout(
-            direction = Direction.Horizontal,
-            constraints = Array(Constraint.Percentage(30), Constraint.Percentage(50)),
-          ).split(f.size).tap { columns =>
-            val list = ListWidget(
-              block = Some(
-                BlockWidget(
-                  title = Some(Spans(Array(Span("select your poison", Style.DEFAULT)))),
-                  borders = Borders.ALL,
-                )
-              ),
-              items =
-                items
-                  .map(i =>
-                    ListWidget.Item(
-                      content = Text(
-                        Array(
-                          Spans(
-                            Array(Span(i, Style.DEFAULT))
+          val now = Instant.now()
+
+          val item = items(selected)
+
+          terminal.draw { f =>
+            Layout(
+              direction = Direction.Horizontal,
+              constraints = Array(Constraint.Percentage(30), Constraint.Percentage(50)),
+            ).split(f.size).tap { columns =>
+              val list = ListWidget(
+                block = Some(
+                  BlockWidget(
+                    title = Some(
+                      Spans(Array(Span("select your poison", Style.DEFAULT)))
+                    ),
+                    borders = Borders.ALL,
+                  )
+                ),
+                items =
+                  items
+                    .map(i =>
+                      ListWidget.Item(
+                        content = Text(
+                          Array(
+                            Spans(
+                              Array(Span(i, Style.DEFAULT))
+                            )
                           )
                         )
                       )
                     )
-                  )
-                  .toArray,
-              style = Style.DEFAULT,
-              startCorner = Corner.TopLeft,
-              highlightStyle = Style.DEFAULT.fg(Color.Red),
-              highlightSymbol = Some("> "),
-            )
-
-            f.renderStatefulWidget(
-              list,
-              columns(0),
-            )(ListWidget.State(offset = 0, selected = Some(selected)))
-
-            Layout(
-              direction = Direction.Vertical,
-              constraints = Array(Constraint.Length(1), Constraint.Min(1)),
-              margin = Margin(1),
-            ).split(columns(1)).pipe { rhsRows =>
-              f.renderWidget(
-                BlockWidget(
-                  title = Some(Spans(Array(Span("details", Style.DEFAULT)))),
-                  borders = Borders.ALL,
-                  borderStyle = Style.DEFAULT.fg(Color.Green),
-                ),
-                columns(1),
+                    .toArray,
+                style = Style.DEFAULT,
+                startCorner = Corner.TopLeft,
+                highlightStyle = Style.DEFAULT.fg(Color.Red),
+                highlightSymbol = Some("> "),
               )
 
-              val header = ParagraphWidget(
-                text = Text(
-                  Array(
-                    Spans(Array(Span(s"see all the details about ${item} here", Style.DEFAULT)))
+              f.renderStatefulWidget(
+                list,
+                columns(0),
+              )(ListWidget.State(offset = 0, selected = Some(selected)))
+
+              Layout(
+                direction = Direction.Vertical,
+                constraints = Array(Constraint.Length(1), Constraint.Min(1)),
+                margin = Margin(1),
+              ).split(columns(1)).pipe { rhsRows =>
+                f.renderWidget(
+                  BlockWidget(
+                    title = Some(Spans(Array(Span(s"details (time is ${now}", Style.DEFAULT)))),
+                    borders = Borders.ALL,
+                    borderStyle = Style.DEFAULT.fg(Color.Green),
+                  ),
+                  columns(1),
+                )
+
+                val header = ParagraphWidget(
+                  text = Text(
+                    Array(
+                      Spans(Array(Span(s"see all the details about ${item} here", Style.DEFAULT)))
+                    )
                   )
                 )
-              )
 
-              val deets = ParagraphWidget(
-                text = Text.nostyle(
-                  s"oh yes this is the details! Let me tell you the full story of lorem ipsum, friends, adventure and betrayal.\n\nmaybe even some more things too."
-                ),
-                wrap = Some(Wrap(trim = false)),
-              )
+                val deets = ParagraphWidget(
+                  text = Text.nostyle(
+                    s"oh yes this is the details! Let me tell you the full story of lorem ipsum, friends, adventure and betrayal.\n\nmaybe even some more things too."
+                  ),
+                  wrap = Some(Wrap(trim = false)),
+                )
 
-              f.renderWidget(header, rhsRows(0))
+                f.renderWidget(header, rhsRows(0))
 
-              f.renderWidget(
-                deets,
-                rhsRows(1).inner(Margin(1, 0)),
-              )
+                f.renderWidget(
+                  deets,
+                  rhsRows(1).inner(Margin(1, 0)),
+                )
 
+              }
             }
           }
         }
 
-        jni.read() match {
-          case key: tui.crossterm.Event.Key =>
-            key.keyEvent.code match {
-              case char: tui.crossterm.KeyCode.Char
-                  if char.c() == 'q' || (char
-                    .c() == 'c' && (key.keyEvent().modifiers().bits() == KeyModifiers.CONTROL)) =>
-                util.boundary.break(None)
-              case char: tui.crossterm.KeyCode.Char if char.c() == 'n' =>
-                items :+= s"NEW ITEM! ${UUID.randomUUID()}"
-              case char: tui.crossterm.KeyCode.Up =>
-                selected = (selected - 1).clamped(items.indices)
-              case char: tui.crossterm.KeyCode.Down =>
-                selected = (selected + 1).clamped(items.indices)
-              case keys: tui.crossterm.KeyCode.Enter => util.boundary.break(Some(items(selected)))
-              case _                                 => ()
-            }
-          case _ => ()
+      def eventLoop = {
+        channel.send(Event.Tick)
+        while (true) {
+          jni.read() match {
+            case key: tui.crossterm.Event.Key =>
+              key.keyEvent.code match {
+                case char: tui.crossterm.KeyCode.Char
+                    if char.c() == 'q' || (char
+                      .c() == 'c' && (key.keyEvent().modifiers().bits() == KeyModifiers.CONTROL)) =>
+                  util.boundary.break(None)
+                case char: tui.crossterm.KeyCode.Char if char.c() == 'n' =>
+                  items :+= s"NEW ITEM! ${UUID.randomUUID()}"
+                case char: tui.crossterm.KeyCode.Up =>
+                  selected = (selected - 1).clamped(items.indices)
+                case char: tui.crossterm.KeyCode.Down =>
+                  selected = (selected + 1).clamped(items.indices)
+                case keys: tui.crossterm.KeyCode.Enter => util.boundary.break(Some(items(selected)))
+                case _                                 => ()
+              }
+            case _ => ()
+          }
+          channel.send(Event.Tick)
         }
-      }
-    }
 
-    None
+        None
+      }
+
+      def clock =
+        while (true) {
+          ox.sleep(1.second)
+          channel.send(Event.Tick)
+        }
+
+      ox.par(renderLoop, eventLoop, clock)._2
+    }
   }
 
   choice.foreach { c =>
