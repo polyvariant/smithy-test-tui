10a6a56249c7f69b16b8e176f945b9b3e24f09a5
Jakub Kozłowski <kubukoz@gmail.com>
2026-05-06 14:39:51 +0200

    Wire passthru.tests into flake checks via collectChecks

--- flake.nix
+++ flake.nix
@@ -1,11 +1,11 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
-    scala-cli-nix.url = "github:scala-nix/scala-cli-nix";
+    scala-cli-nix.url = "github:scala-nix/scala-cli-nix/native-support";
     scala-cli-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { nixpkgs, scala-cli-nix, ... }:
+  outputs = { self, nixpkgs, scala-cli-nix, ... }:
     let
       forAllSystems = nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-darwin" "x86_64-darwin" ];
     in {
@@ -18,6 +18,17 @@
         in {
           default = pkgs.callPackage ./derivation.nix { };
         }
+      );
+
+      # Pull `passthru.tests` from every package into checks, so
+      # `nix flake check` runs the test scope of each target.
+      checks = forAllSystems (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ scala-cli-nix.overlays.default ];
+          };
+        in pkgs.scala-cli-nix.collectChecks self.packages.${system}
       );
 
       devShells = forAllSystems (system:
